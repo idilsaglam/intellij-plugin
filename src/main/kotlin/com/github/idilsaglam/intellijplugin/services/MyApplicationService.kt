2a84e0e6abedfac0b2c745756a031372277d9919
GitHub Action <action@github.com>
2022-03-30 10:46:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/idilsaglam/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/idilsaglam/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.idilsaglam.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.idilsaglam.intellijplugin.MyBundle
 
 class MyApplicationService {
 
